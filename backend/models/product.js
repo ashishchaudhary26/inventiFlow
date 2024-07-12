bd0692efd5a0606e2a80e784fd9477cd71845332
ashishchaudhary26 <chaudharyash2002@gmail.com>
2024-07-12 22:00:24 +0530

    commmiiting all models

---
+++ backend/models/product.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    manufacturer: {
+      type: String,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    description: String,
+  },
+  { timestamps: true }
+);
